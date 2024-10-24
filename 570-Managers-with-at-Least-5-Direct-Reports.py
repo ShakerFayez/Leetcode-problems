571db342082266801927ed4470feae25bf6768c4
Shaker Fayez <80179621+ShakerFayez@users.noreply.github.com>
2024-10-25 00:38:41 +0300

    [570] [Time Beats: 21.53%] - LeetPush

---
+++ 570-Managers-with-at-Least-5-Direct-Reports.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    managers = employee.groupby('managerId', as_index=False).agg(reporting=('id', 'count')).query('reporting >= 5')['managerId']
+
+    return employee[employee['id'].isin(managers)][['name']]
