a2b6824cac26e9a51b51959ce6401e5266fd6f74
Shaker Fayez <80179621+ShakerFayez@users.noreply.github.com>
2024-10-29 12:39:18 +0300

    [1633] [Time Beats: 83.10%] - LeetPush

---
+++ 1633-Percentage-of-Users-Attended-a-Contest.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def users_percentage(users: pd.DataFrame, register: pd.DataFrame) -> pd.DataFrame:
+    df = register.groupby('contest_id', as_index=False).agg(count=('user_id','nunique'))
+    df['percentage'] = df['count']/len(users)*100
+    result = df[['contest_id','percentage']].sort_values(by=['percentage', 'contest_id'], ascending=[False, True]).round(2)
+    return result
