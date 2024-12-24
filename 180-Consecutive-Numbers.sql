abc468a323848cc87b89baf326fe3210d501cff1
Shaker Fayez <80179621+ShakerFayez@users.noreply.github.com>
2024-12-25 00:01:05 +0200

    [180] [Time Beats: 5.10%] - LeetPush

---
+++ 180-Consecutive-Numbers.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT
+    num AS ConsecutiveNums
+FROM
+    (
+        SELECT 
+            num,
+            LAG(num, 1) OVER (ORDER BY id) as prev1,
+            LAG(num, 2) OVER (ORDER BY id) as prev2
+        FROM
+            Logs
+    ) AS subquery
+WHERE
+    num = prev1 AND num = prev2
