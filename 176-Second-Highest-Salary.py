fb9aa9ac2a0272671a8be3140be872ede94dfe3e
Shaker Fayez <80179621+ShakerFayez@users.noreply.github.com>
2025-01-19 22:09:00 +0200

    [176] [Time Beats: 35.04%] - LeetPush

---
+++ 176-Second-Highest-Salary.py
@@ -0,0 +1,7 @@
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    sorted_salaries = employee['salary'].sort_values(
+        ascending=False
+    ).drop_duplicates()
+    return pd.DataFrame({
+        'SecondHighestSalary': [None if sorted_salaries.size < 2 else sorted_salaries.iloc[1]]
+    })
